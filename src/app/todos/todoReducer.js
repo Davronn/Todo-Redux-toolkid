71f28bbae7fe7b12e7953493dcf5567cf4d03f7c
Davronn <davronmahamadjonov8@gmai.com>
2024-04-17 17:27:54 +0500

    .

--- src/app/todos/todoReducer.js
+++ src/app/todos/todoReducer.js
@@ -1,5 +1,3 @@
-// In todoReducer.js
-
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
@@ -7,14 +5,23 @@ export const fetchTodos = createAsyncThunk("todo/fetchTodos", () =>
   axios
     .get("http://localhost:3000/todos")
     .then((res) => res.data)
-    .catch((err) => err.message)
+    .catch((err) => {
+      throw err;
+    })
 );
 
-export const toggleTodo = createAsyncThunk("todo/toggleTodo", (id) =>
-  axios
-    .patch(`http://localhost:3000/todos/${id}`, )
-    .then((res) => res.data)
-    .catch((err) => err.message)
+export const toggleTodo = createAsyncThunk(
+  "todo/toggleTodo",
+  async (id, { getState }) => {
+    const { todos } = getState().todo;
+    const todo = todos.find((todo) => todo.id === id);
+    if (!todo) {
+      throw new Error("Todo not found");
+    }
+    const updatedTodo = { ...todo, completed: !todo.completed };
+    await axios.patch(`http://localhost:3000/todos/${id}`, updatedTodo);
+    return updatedTodo;
+  }
 );
 
 export const addTodo = createAsyncThunk("todo/addTodo", async (todoData) => {
@@ -32,36 +39,35 @@ const todo = createSlice({
   initialState: {
     loading: false,
     todos: [],
-    error: "",
+    error: null,
   },
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(fetchTodos.pending, (state) => {
-      state.loading = true;
-      state.todos = [];
-      state.error = "";
-    });
-    builder.addCase(fetchTodos.fulfilled, (state, action) => {
-      state.loading = false;
-      state.todos = action.payload;
-      state.error = "";
-    });
-    builder.addCase(fetchTodos.rejected, (state, action) => {
-      state.loading = false;
-      state.todos = [];
-      state.error = action.payload;
-    });
-    builder.addCase(toggleTodo.fulfilled, (state, action) => {
-      const updatedTodos = state.todos.map((todo) =>
-        todo.id === action.payload.id
-          ? { ...todo, completed: !todo.completed }
-          : {...todo, completed: todo.completed}
-      );
-      state.todos = updatedTodos;
-    });
-    builder.addCase(deleteTodo.fulfilled, (state, action) => {
-      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
-    });
+    builder
+      .addCase(fetchTodos.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchTodos.fulfilled, (state, action) => {
+        state.loading = false;
+        state.todos = action.payload;
+      })
+      .addCase(fetchTodos.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+      .addCase(toggleTodo.fulfilled, (state, action) => {
+        const updatedTodo = action.payload;
+        const index = state.todos.findIndex(
+          (todo) => todo.id === updatedTodo.id
+        );
+        if (index !== -1) {
+          state.todos[index] = updatedTodo;
+        }
+      })
+      .addCase(deleteTodo.fulfilled, (state, action) => {
+        state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+      });
   },
 });
 
